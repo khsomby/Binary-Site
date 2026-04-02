b365887caad6f41e07757a623ad438341d24c44f
Somby Ny Aina <137616969+khsomby@users.noreply.github.com>
2026-04-02 19:02:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://your-firebase-project.firebaseio.com"
+  databaseURL: "https://somby-1700653034400-default-rtdb.firebaseio.com"
 });
 
 const db = admin.firestore();
